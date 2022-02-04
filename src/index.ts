3936b17cb63db9bb91426ddee243f2c0592571ce
Gabriel <>
2022-02-04 12:29:45 -0300

    prettier lol

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-require('dotenv').config()
-import express from 'express'
-import cors from 'cors'
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
 
-import routes from './routes'
+import routes from "./routes";
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors({origin: process.env.ALLOW_ORIGIN}))
-app.use(routes)
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: process.env.ALLOW_ORIGIN }));
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
-    console.log('http://localhost:5000/')
-})
-
+	console.log("Servidor foi iniciado");
+});
--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -1,19 +1,19 @@
-import type {Response, Request, NextFunction, Router} from 'express'
-import {verifyAuth} from '../auth'
+import type { Response, Request, NextFunction, Router } from "express";
+import { verifyAuth } from "../auth";
 
 type createUserBody = {
-    username:string;
-    password:string;
-    email:string;
-}
+	username: string;
+	password: string;
+	email: string;
+};
 
-const createUser = (req:Request, res:Response) => {
-    const {username, email, password} = req.body as createUserBody
-    res.send('ok')
-}
+const createUser = (req: Request, res: Response) => {
+	const { username, email, password } = req.body as createUserBody;
+	res.send("ok");
+};
 
-const user = (routes:Router) => {
-    routes.use('/create', verifyAuth(createUser))
-}
+const user = (routes: Router) => {
+	routes.use("/create", verifyAuth(createUser));
+};
 
-export default user
+export default user;

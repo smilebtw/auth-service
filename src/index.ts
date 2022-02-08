ab6c33887e4775297605c2efd8eee64a196beb36
Gabriel <>
2022-02-08 12:35:01 -0300

    feat: criação das rotas login e query dos usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 require("dotenv").config();
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import routes from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors({ origin: process.env.ALLOW_ORIGIN }));
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
-	console.log("Servidor foi iniciado");
+  console.log("Servidor foi iniciado");
 });
--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -1,16 +1,18 @@
+require("dotenv").config();
 import type { Response, Request, NextFunction, Router } from "express";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import database from "../../database/index";
 import { Prisma } from "@prisma/client";
 
-type createUserBody = {
+type userBody = {
   username: string;
   password: string;
   email: string;
 };
 
 const createUser = async (req: Request, res: Response) => {
-  const { username, email, password } = req.body as createUserBody;
+  const { username, email, password } = req.body as userBody;
   if (!username || !email || !password) {
     return res.status(400).json({
       error: {
@@ -44,8 +46,78 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const loginUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body as userBody;
+  if (!username || !password) {
+    return res.status(400).json({
+      error: {
+        message: "Check your request body",
+      },
+    });
+  }
+
+  const user = await database.user.findFirst({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!user)
+    res.status(404).json({
+      error: {
+        message: "User not found in database",
+      },
+    });
+  else {
+    const secret = process.env.JWT_SECRET;
+    const isValid = await bcrypt.compare(password, user.password);
+    if (isValid && secret) {
+      const token = jwt.sign({ role: user.role }, secret, {
+        expiresIn: "8h",
+      });
+      res.cookie("access_token", token).status(200).json({
+        message: "Logged in successfully",
+      });
+    }
+  }
+};
+
+const getAllUsers = async (req: Request, res: Response) => {
+  const users = await database.user.findMany();
+  if (!users) {
+    res.status(404).json({
+      error: {
+        message: "No users were found",
+      },
+    });
+  } else {
+    res.json(users);
+  }
+};
+
+const getOneUser = async (req: Request, res: Response) => {
+  const user = await database.user.findFirst({
+    where: {
+      username: req.params.user,
+    },
+  });
+
+  if (!user) {
+    res.status(404).json({
+      error: {
+        message: "User not found",
+      },
+    });
+  } else {
+    res.json(user);
+  }
+};
+
 const user = (routes: Router) => {
   routes.post("/v1/auth/create", createUser);
+  routes.post("/v1/auth/login", loginUser);
+  routes.get("/v1/auth/user", getAllUsers);
+  routes.get("/v1/auth/user/:user", getOneUser);
 };
 
 export default user;
